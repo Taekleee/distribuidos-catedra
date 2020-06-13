5b73f15e4556fb08d889d6ae25fb5fb881f46bde
Cata <Catalina.morales.r@usach.cl>
2020-06-12 22:00:44 -0400

    initial

---
+++ src/main/java/com/comisaria/distribuidos/DistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.comisaria.distribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistribuidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comisaria/distribuidos/DistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comisaria.distribuidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
